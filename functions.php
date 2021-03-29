65977f53bd6bd59be45377674786fd5dacae22c0
coatriver <sylvain.jacot@hotmail.be>
2021-03-29 16:49:46 +0200

    defer for js added

--- functions.php
+++ functions.php
@@ -21,4 +21,13 @@ function silvano_scripts() {
 
 add_action( 'wp_enqueue_scripts', 'silvano_scripts');
 
+// Add defer attribute to js files
+function defer_parsing_of_js ( $url ) {
+    if ( FALSE === strpos( $url, '.js' ) ) return $url;
+    if ( strpos( $url, 'jquery.js' ) ) return $url;
+    return "$url' defer ";
+    }
+add_filter( 'clean_url', 'defer_parsing_of_js', 11, 1 );  
+    
+
 
